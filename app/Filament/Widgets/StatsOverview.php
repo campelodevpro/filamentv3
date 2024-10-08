be79ac0cfaa0b3634e2ebf059142b4503845869a
Campelo Dev Pro <campelodevpro>
2024-10-07 23:42:42 -0300

    Varios cards de Stats no dashboard

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Widgets;
 
 use App\Models\User;
+use Filament\Support\Enums\IconPosition;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -11,10 +12,61 @@ class StatsOverview extends BaseWidget
     protected function getStats(): array
     {
         return [
-            Stat::make('Total de Usuários', User::query()->count()),
+
+            Stat::make('Unique views', '192.1k'),
+            Stat::make('Bounce rate', '21%'),
+            Stat::make('Average time on page', '3:12'),
+
+            Stat::make('Unique views', '192.1k')
+            ->description('32k increase')
+            ->descriptionIcon('heroicon-m-arrow-trending-up'),
+            Stat::make('Bounce rate', '21%')
+            ->description('7% decrease')
+            ->descriptionIcon('heroicon-m-arrow-trending-down'),
+            Stat::make('Average time on page', '3:12')
+            ->description('3% increase')
+            ->descriptionIcon('heroicon-m-arrow-trending-up'),
+
+            Stat::make('Unique views', '192.1k')
+            ->description('32k increase')
+            ->descriptionIcon('heroicon-m-arrow-trending-up', IconPosition::Before),
+
+            Stat::make('Unique views', '192.1k')
+            ->description('32k increase')
+            ->descriptionIcon('heroicon-m-arrow-trending-up')
+            ->color('success'),
+            Stat::make('Bounce rate', '21%')
+            ->description('7% increase')
+            ->descriptionIcon('heroicon-m-arrow-trending-down')
+            ->color('danger'),
+            Stat::make('Average time on page', '3:88')
+            ->description('3% increase')
+            ->descriptionIcon('heroicon-m-arrow-trending-up')
+            ->color('success'),
+
+            Stat::make('Processed', '192.1k')
+            ->color('success')
+            ->extraAttributes([
+                'class' => 'cursor-pointer',
+                'wire:click' => "\$dispatch('setStatusFilter', { filter: 'processed' })",
+            ]),
+
+            Stat::make('Total de Usuários', User::query()->count())
+            ->description('32k increase')
+            ->descriptionIcon('heroicon-m-arrow-trending-up'),
             Stat::make('Total de Usuarios Administradores', User::query()->where('is_admin', true)->count())
-            // Stat::make('Bounce rate', '21%'),
-            // Stat::make('Average time on page', '3:12'),
+            ->description('3% increase')
+            ->descriptionIcon('heroicon-m-arrow-trending-up', IconPosition::Before),
+
+            Stat::make('Total de Usuários', User::query()->count())
+            ->color('success'),
+
+            Stat::make('Unique views', '192.1k')
+            ->description('32k increase')
+            ->descriptionIcon('heroicon-m-arrow-trending-up')
+            ->chart([7, 2, 10, 3, 15, 4, 17])
+            ->color('success'),
+            
         ];
     }
 }
