ca4381beb5684ca299256459f9d10a74f65308d6
Campelo Dev Pro <campelodevpro>
2024-10-07 22:35:54 -0300

    Filmanet insalado e logando

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        \App\Models\User::factory()->create([
+            'name' => 'leo',
+            'email' => 'leo@test.com',
+            'password'=> Hash::make('11111111'),
+            'is_admin'=> true,
+        ]);
+
+        User::factory(35)->create();
 
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'test@example.com'
         ]);
     }
 }
